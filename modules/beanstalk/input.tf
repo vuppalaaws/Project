e08d19c182b2acd9b8ae393ec19d7bf2f08dcff3
vuppalaaws <vuppala.aws@gmail.com>
2020-03-11 23:13:13 -0500

    updated variables

--- modules/beanstalk/input.tf
+++ modules/beanstalk/input.tf
@@ -1,34 +1,20 @@
 #variable "application_name" {}
 #variable "runtime_environment" {}
 variable "name" {}
-
 variable "account_name" {}
-
 variable "vpc_id" {}
 variable "region" {}
 #variable "s3bucket" {}
 variable "ec2_subnet_ids" {}
 variable "elb_subnet_ids" {}
 variable "beanstalk_service_role" {}
-
-
 variable "beanstalk_ec2_role" {}
 variable "product_name" {}
-
-
-
 variable "min_instances" {}
-
 variable "max_instances" {}
-
 variable "key_name" {}
-
 variable "ec2_instance_type" {}
-
-
-
 variable "ec2_security_group_ids" {}
-
 variable "wait_for_ready_timeout" {}
 
 
